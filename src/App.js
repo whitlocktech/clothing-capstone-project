a26eada16b7ef78dfd34a28aa6efd1f848fd4569
whitlocktech <whitlocktech@gmail.com>
2023-05-19 16:44:22 -0500

    component changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './routes/home/home.component.jsx'
+import Navigation from './routes/navigation/navigation.component.jsx'
+
+const Shop = () => {
+  return (
+    <div>
+      I am the Shop element.
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />}>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
   )
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import './footer.styles.scss'
+
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            <p>whitlocktech Zero To Master React 2023 Course Capstone Project</p>
+        </div>
+    )
+}
+
+export default Footer
